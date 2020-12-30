569197faff81cb276140a946f7529655265831fd
wisemandaniel <58503431+wisemandaniel@users.noreply.github.com>
2020-12-30 19:03:04 +0100

    livewire first component added, the counter to counts up and down

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div style = "text-align: center; margin-top: 200px">
+   <span>{{ $count }}</span>
+   <button wire:click="decrement" style = "background-color: blue;font-size: 30px; width: 50px;">-</button>
+   <button wire:click="increment" style = "background-color: blue;font-size: 30px; width: 50px;">+</button>
+</div>
